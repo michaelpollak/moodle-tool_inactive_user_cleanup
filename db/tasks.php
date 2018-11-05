85e6ba865d83a6ea3a62b6a67991236e19a2a662
michaelpollak <github@michaelpollak.org>
2018-11-05 10:13:55 +0100

    Changed cron to just once a month.

--- db/tasks.php
+++ db/tasks.php
@@ -1,12 +1,12 @@
 <?php
-$tasks = array(                                                                                                                     
-    array(                                                                                                                          
-        'classname' => 'tool_inactive_user_cleanup\task\tool_inactive_user_cleanup_task',                                                                            
-        'blocking' => 0,                                                                                                            
-        'minute' => '59',                                                                                                            
-        'hour' => '23',                                                                                                              
-        'day' => '*',                                                                                                               
-        'dayofweek' => '*',                                                                                                         
-        'month' => '*'                                                                                                              
+$tasks = array(
+    array(
+        'classname' => 'tool_inactive_user_cleanup\task\tool_inactive_user_cleanup_task',
+        'blocking' => 0,
+        'minute' => '59',
+        'hour' => '2',
+        'day' => '3',
+        'dayofweek' => '*',
+        'month' => '*'
     )
 );
